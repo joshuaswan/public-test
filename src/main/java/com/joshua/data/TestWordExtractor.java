e85bc71f71318228abbc130d12fcb9e728038915
joshuaswan <joshuaswan@sina.com>
2018-05-23 17:34:31 +0800

    修改文件结构，将后台服务单独放在模块中，之后在根路径下创建前台项目

--- src/main/java/com/joshua/data/TestWordExtractor.java
+++
@@ -1,7 +0,0 @@
-package com.joshua.data;
-
-/**
- * Created by joshua on 2016/5/1.
- */
-public class TestWordExtractor {
-}
--- src/main/java/com/joshua/test/AutoTest.java
+++
@@ -1,7 +0,0 @@
-package com.joshua.test;
-
-/**
- * Created by joshua on 2018-05-15.
- */
-public class AutoTest {
-}
